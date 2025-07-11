e0ea8b3c3bd0e874fdd91a042e3ed3a1f94734d3
Harsh <bhonsleharsh16@gmail.com>
2025-07-11 17:07:08 +0530

    fix typo in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './Components/Header'
 import About from './Components/About'
 import './App.css'
-import HeroHeading from './Components/heroHeading'
+import HeroHeading from './Components/HeroHeading'
 import Service from './Components/Service'
 import Recent from "./Components/Recent"
 import Contact from './Components/Contact'
